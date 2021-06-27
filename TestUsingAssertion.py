7db574450351808f190497e677975ae7320c94a0
Priya Singh <35927218+singhpia@users.noreply.github.com>
2021-06-28 02:04:08 +0530

    Update and rename pract sel.py to TestUsingAssertion.py

--- TestUsingAssertion.py
+++ TestUsingAssertion.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
 
+#instantiate the chrome webdriver
 driver = webdriver.Chrome()
 
+#request to navigate to google.com
 driver.get("https://google.com")
+
+#testing if the title of the page is Google and using try, except just to handle exception
 assert driver.title=="Google"
 try:
     if driver.title == "Google":
